8669b17b32a381725d3d29eee698d9fd76d4b41d
Eduardo Martini <123970080+duudumartini@users.noreply.github.com>
2024-05-28 17:50:00 +0000

    Update

--- Portal-Home/JS/controla_frase_do_dia.js
+++ Portal-Home/JS/controla_frase_do_dia.js
@@ -2,8 +2,18 @@ let urlFrase = `https://api.adviceslip.com/advice`;
 
 fetch(urlFrase)
   .then(response => response.json())
-  .then(data =>{
+  .then(data => {
     let frase = document.getElementById("frase_do_dia");
-    frase.innerHTML = `"${data.slip.advice}"`;
-})
-.catch(error => console.log('Ocorreu um erro:', error));
+    let textoOriginal = data.slip.advice;
+    let urlTraducao = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=pt&dt=t&q=" + encodeURI(textoOriginal);
+    
+    fetch(urlTraducao)
+      .then(response => response.json())
+      .then(data => {
+        // Extrai o texto traduzido da resposta
+        let textoTraduzido = data[0][0][0];
+        frase.innerHTML = `"${textoTraduzido}"`;
+      })
+      .catch(error => console.error('Erro ao traduzir:', error));
+  })
+  .catch(error => console.log('Ocorreu um erro:', error));
